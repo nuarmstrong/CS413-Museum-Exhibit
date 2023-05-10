8763ea961012ea74dcff9e337c519486011e2bf1
Ian Armstrong <ian@A-Better-MacBook-Pro.local>
2023-05-09 19:23:36 -0500

    functional for 1 input

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,38 @@
 #include <Arduino.h>
 #include <FastLED.h>
-#include <Location.h>
+#include "Location.h"
 
 #define NUM_LEDS 256
 #define DATA_PIN 3
 
 CRGB leds[NUM_LEDS];
 
-void light(Location loc, CRGB color = CRGB::Red);
+void light(Location loc, CRGB color = CRGB::Red) {
+  uint16_t index = loc.get_index();
+  leds[index] = color;
+  FastLED.show();
+  delay(1000);
+}
+
+uint16_t get_index(uint8_t x, uint8_t y) {
+    if (y & 0x01) {
+        uint8_t reversedX = (16 - 1) - x;
+        return (y * 16) + reversedX;
+    } else {
+        return (y * 16) + x;
+    }
+  }
 
 Location start = Location(7, 8);
 
 void setup() { 
   randomSeed(analogRead(0));
   FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);  // GRB ordering is typical
-  FastLED.setBrightness(1);
+  FastLED.setBrightness(5);
   FastLED.clear();
+  Serial.begin(9600);
 }
+
 
 void loop() { 
   bool written[256];
@@ -26,26 +42,23 @@ void loop() {
     int old_index = start.get_index();
     written[old_index] = 1;
     light(start);
+    Serial.print("Found Location: ");
+    Serial.println(start.get_index());
 
-    while (true) { // keep iterating until we find an unwritten index
+    while (written[start.get_index()]) {
       start.get_next_location();
-      int new_index = start.get_index();
-      if (!written[new_index]) {
-        break; // found an unwritten index, break out of the loop
-      }
+      Serial.print("SKIPPING ");
+      Serial.println(start.get_index());
     }
+
+    // written[start.get_index()] = true;
   }
   delay(5000);
   FastLED.clear();
   delay(100);
 }
 
-void light(Location loc, CRGB color = CRGB::Red) {
-  uint16_t index = loc.get_index();
-  leds[index] = color;
-  FastLED.show();
-  delay(1000);
-}
+
 
 
 
