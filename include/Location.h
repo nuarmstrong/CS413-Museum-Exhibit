b1f2d9a7a6deba987aa227a7ed42a42d1678fe48
Ian Armstrong <armstrong.ian.m@gmail.com>
2023-06-07 10:30:15 -0500

    cleanup and comments

--- include/Location.h
+++ include/Location.h
@@ -1,7 +1,7 @@
 #ifndef LOCATION_H
 #define LOCATION_H
 
-#define COORD_MAX 15
+#define COORD_MAX 16
 #define COORD_MIN 0
 
 #include <Arduino.h>
@@ -13,38 +13,40 @@ class Location {
 
   public:
 
-    Location() : Location(random() % 16, random() % 16) { }
+    Location() : Location(random() % COORD_MAX, random() % COORD_MAX) { }
 
     Location(uint8_t x, uint8_t y) : x(x), y(y) { }
 
     ~Location() { }
 
     void get_next_location() {
-        int dx = random(3) - 1; // -1, 0, or 1
-        int dy = random(3) - 1; // -1, 0, or 1
+        int dx = random(3) - 1; // -1, COORD_MIN, or 1
+        int dy = random(3) - 1; // -1, COORD_MIN, or 1
 
         // Ensure that the move changes at least one coordinate.
-        while(dx == 0 && dy == 0){
+        while(dx == COORD_MIN && dy == COORD_MIN){
             dx = random(3) - 1;
             dy = random(3) - 1;
         }
 
         // Check boundaries and adjust coordinates
-        if(x + dx >= 0 && x + dx < 16){
+        if(x + dx >= COORD_MIN && x + dx < COORD_MAX){
             x += dx;
         }
 
-        if(y + dy >= 0 && y + dy < 16){
+        if(y + dy >= COORD_MIN && y + dy < COORD_MAX){
             y += dy;
         }
     }
 
+    // Calculate the index of the location in the 1D array
+    // given the 2D coordinates.
     uint16_t get_index() {
       if (this->x & 0x01) {
-          uint8_t reversedY = (16 - 1) - y;
-          return (x * 16) + reversedY;
+          uint8_t reversedY = (COORD_MAX - 1) - y;
+          return (x * COORD_MAX) + reversedY;
       } else {
-          return (x * 16) + y;
+          return (x * COORD_MAX) + y;
       }
     }
 };
